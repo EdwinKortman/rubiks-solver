f9828c399072516784f81e8290bd385c0e159638
Edwin Kortman <edwin.kortman@tamtam.nl>
2016-06-19 13:12:08 +0200

    Adds basic set up for cube

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 #include <vector>
 
+class Cube
+{
+    private:
+        int edgeSize;
+
+    public:
+        void setEdge(int size);
+        int getEdge(void);
+        Cube(int size);
+};
+
+
+Cube::Cube(int size)
+{
+    edgeSize = size;
+}
+
+void Cube::setEdge(int size)
+{
+    edgeSize = size;
+}
+
+int Cube::getEdge(void)
+{
+    return edgeSize;
+}
+
 
 int main(int argc, char **argv)
 {
-
+    Cube Cube(3);
+    std::cout << "Size of edge: " << Cube.getEdge() << std::endl;
 }
